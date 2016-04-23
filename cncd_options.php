71e14a73d27146b2dc760cebff5d6c34716d1122
Rainer <rainer@websimple.be>
2016-04-23 09:56:56 +0200

    import initial

---
+++ cncd_options.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Options du plugin CNCDau chargement
+ *
+ * @plugin     CNCD
+ * @copyright  2016
+ * @author     rainer
+ * @licence    GNU/GPL
+ * @package    SPIP\Cncd\Options
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/*
+ * Un fichier d'options permet de définir des éléments
+ * systématiquement chargés à chaque hit sur SPIP.
+ *
+ * Il vaut donc mieux limiter au maximum son usage
+ * tout comme son volume !
+ * 
+ */
+
+?>
---
+++ cncd_pipelines.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Utilisations de pipelines par CNCD
+ *
+ * @plugin     CNCD
+ * @copyright  2016
+ * @author     rainer
+ * @licence    GNU/GPL
+ * @package    SPIP\Cncd\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
+
+
+
+
+?>
