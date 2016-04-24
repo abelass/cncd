284e1211a3fb8fe793c9f3715b25839021bec5c6
Rainer Müller <rainer@websimple.be>
2016-04-24 17:35:48 +0200

    autorisation pour créer l'évenement

--- cncd_fonctions.php
+++ cncd_fonctions.php
@@ -12,12 +12,26 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
-/*
- * Un fichier de fonctions permet de définir des éléments
- * systématiquement chargés lors du calcul des squelettes.
- *
- * Il peut par exemple définir des filtres, critères, balises, …
- * 
- */
-
-?>
+function autoriser_article_creerevenementdans($faire,$quoi,$id,$qui,$options){
+	if (!$id) return false; // interdit de creer un evenement sur un article vide !
+	// si on a le droit de modifier l'article alors on a peut-etre le droit d'y creer un evenement
+	$afficher = false;
+	if (autoriser('modifier','article',$id,$qui)) {
+		$afficher = true;
+		// un article avec des evenements a toujours le droit
+		if (!sql_countsel('spip_evenements','id_article='.intval($id))){
+			// si au moins une rubrique a le flag agenda
+			if (sql_countsel('spip_rubriques','agenda=1')){
+				// alors il faut le flag agenda dans cette branche !
+				$afficher = false;
+				include_spip('inc/rubriques');
+				$in = calcul_hierarchie_in(sql_getfetsel('id_rubrique','spip_articles','id_article='.intval($id)));
+				$afficher = sql_countsel('spip_rubriques',sql_in('id_rubrique',$in)." AND agenda=1");
+			}
+		}
+	}
+	elseif (!_request('exec')) {
+		$afficher = true;
+	}
+	return $afficher;
+}
