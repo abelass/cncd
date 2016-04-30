3aa7d1950dec2baf468720b1509ca25fad1b82da
Rainer <rainer@websimple.be>
2016-04-30 09:39:47 +0200

    proteger le formulaire avec nospam

--- cncd_options.php
+++ cncd_options.php
@@ -11,14 +11,7 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-
-/*
- * Un fichier d'options permet de définir des éléments
- * systématiquement chargés à chaque hit sur SPIP.
- *
- * Il vaut donc mieux limiter au maximum son usage
- * tout comme son volume !
- * 
- */
+// proteger le #FORMULAIRE_EDITER_EVENEMENT
+$GLOBALS['formulaires_no_spam'][] = 'editer_evenement';
 
 ?>
