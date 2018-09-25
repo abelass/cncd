816a46722cf15c2ce3dce40eef77d26826ed6291
abelass <rainer@websimple.be>
2018-09-25 11:46:06 +0200

    format

--- notifications/creation_evenement.php
+++ notifications/creation_evenement.php
@@ -4,21 +4,22 @@ if (! defined ( "_ECRIRE_INC_VERSION" ))
 function notifications_creation_evenement_dist($quoi, $id_evenement, $options) {
 	include_spip ( 'inc/config' );
 	$nom_site = $GLOBALS ['meta'] ['nom_site'];
-	$envoyer_mail = charger_fonction ( 'envoyer_mail', 'inc' );
-	$subject = _T ( 'cncd:notification_sujet_creation_evenement', array (
+	$envoyer_mail = charger_fonction('envoyer_mail', 'inc');
+	$subject = _T ('cncd:notification_sujet_creation_evenement', array (
 			'nom_site' => $nom_site
-	) );
-	$options = array (
-			'id_evenement' => $id_evenement,
-			'qui' => 'visiteur',
-			'email' => $options ['email']
+		)
+	);
+	$options= array(
+		'id_evenement' => $id_evenement,
+		'qui' => 'visiteur',
+		'email' => $options ['email']
 	);
 
-	$message = recuperer_fond ( 'notifications/contenu_creation_evenement_mail', $options );
+	$message = recuperer_fond('notifications/contenu_creation_evenement_mail', $options);
 
 	// Envoyer les emails
 	$o = array (
 			'html' => $message
 	);
-	$envoyer_mail ( $options ['email'], $subject, $o );
+	$envoyer_mail($options['email'], $subject, $o);
 }
